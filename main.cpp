61d4f5a95462adcdb64c7157beac4ac8d762a513
Artem Maltsev <artemmaltsevwork@gmail.com>
2024-12-08 18:48:05 +0200

    Homework#3

--- main.cpp
+++ main.cpp
@@ -1,152 +1,224 @@
 ﻿#include <iostream>
-using namespace std;
+#include <stdexcept>
 
-class MyInt
-{
+// TEMPLATE QUEUE
+template <typename T>
+class Queue {
 private:
-	int value;
+    struct Node {
+        T data;
+        Node* next;
+        Node(T value) : data(value), next(nullptr) {}
+    };
+    Node* front;
+    Node* rear;
+    size_t size;
+
 public:
-	MyInt(int value)
-	{
-		this->value = value;
-	}
-	void SetData(int value)
-	{
-		this->value = value;
-	}
-	int GetData() const
-	{
-		return value;
-	}
+    Queue() : front(nullptr), rear(nullptr), size(0) {}
+    ~Queue() {
+        while (!isEmpty()) {
+            dequeue();
+        }
+    }
+    void enqueue(const T& value) {
+        Node* newNode = new Node(value);
+        if (rear) {
+            rear->next = newNode;
+        }
+        else {
+            front = newNode;
+        }
+        rear = newNode;
+        ++size;
+    }
+    void dequeue() {
+        if (isEmpty()) {
+            std::cout << "Queue is empty";
+        }
+        Node* temp = front;
+        front = front->next;
+        if (!front) {
+            rear = nullptr;
+        }
+        delete temp;
+        --size;
+    }
+    T peek() const {
+        if (isEmpty()) {
+            std::cout << "Queue is empty" << std::endl;
+        }
+        return front->data;
+    }
+    bool isEmpty() const { return size == 0; }
+    size_t getSize() const { return size; }
 };
 
-class Node
-{
+// TEMPLATE TWO LINKED LIST
+template <typename T>
+class LinkedList {
 private:
-	MyInt myInt;
-	Node* next /*= nullptr*/;
+    struct Node {
+        T data;
+        Node* prev;
+        Node* next;
+        Node(T value) : data(value), prev(nullptr), next(nullptr) {}
+    };
+    Node* head;
+    Node* tail;
+    size_t size;
+
 public:
-	Node(MyInt myInt) : myInt(myInt), next(nullptr) {}
-	MyInt GetMyInt() const
-	{
-		return myInt;
-	}
-	void SetMyInt(MyInt myInt)
-	{
-		this->myInt = myInt;
-	}
-	Node* GetNext() const
-	{
-		return next;
-	}
-	void SetNext(Node* next)
-	{
-		this->next = next;
-	}
+    LinkedList() : head(nullptr), tail(nullptr), size(0) {}
+    ~LinkedList() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+    void ShowList() const {
+        Node* temp = head;
+        std::cout << "Show list:" << std::endl;
+        while (temp != nullptr) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+    void pushFront(const T& value) {
+        Node* newNode = new Node(value);
+        newNode->next = head;
+        if (head) {
+            head->prev = newNode;
+        }
+        else {
+            tail = newNode;
+        }
+        head = newNode;
+        ++size;
+    }
+    void pushBack(const T& value) {
+        Node* newNode = new Node(value);
+        newNode->prev = tail;
+        if (tail) {
+            tail->next = newNode;
+        }
+        else {
+            head = newNode;
+        }
+        tail = newNode;
+        ++size;
+    }
+    void popFront() {
+        if (!head) {
+            std::cout << "List is empty";
+        }
+        Node* temp = head;
+        head = head->next;
+        if (head) {
+            head->prev = nullptr;
+        }
+        else {
+            tail = nullptr;
+        }
+        delete temp;
+        --size;
+    }
+    void popBack() {
+        if (!tail) {
+            std::cout << "List is empty";
+        }
+        Node* temp = tail;
+        tail = tail->prev;
+        if (tail) {
+            tail->next = nullptr;
+        }
+        else {
+            head = nullptr;
+        }
+        delete temp;
+        --size;
+    }
+    bool isEmpty() const { return size == 0; }
+    size_t getSize() const { return size; }
 };
 
-class LinkedList
-{
+// TEMPLATE SINGLE LIST
+template <typename T>
+class SingleLinkedList {
 private:
-	Node* head;
-	size_t size;
+    struct Node {
+        T data;
+        Node* next;
+        Node(T value) : data(value), next(nullptr) {}
+    };
+    Node* head;
+    size_t size;
+
 public:
-	LinkedList() : head(nullptr), size(0) {}
-	size_t GetSize() const
-	{
-		return size;
-	}
-	void InsertAtEnd(MyInt value)
-	{
-		if (head == nullptr)
-		{
-			InsertAtBeginning(value);
-			return;
-		}
-		Node* newNode = new Node(value);
+    SingleLinkedList() : head(nullptr), size(0) {}
 
-		Node* temp = head;
-		while (temp->GetNext() != nullptr)
-		{
-			temp = temp->GetNext();
-		}
-		temp->SetNext(newNode);
-		size++;
-	}
-	void InsertAtBeginning(MyInt value)
-	{
-		Node* newNode = new Node(value);
-		newNode->SetNext(head);
-		head = newNode;
-		size++;
-	}
-	void DeleteFromBeginning() {
-		if (head == nullptr) return;
-		Node* temp = head;
-		head = head->GetNext();
-		delete temp;
-		size--;
-	}
+    ~SingleLinkedList() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+    void ShowList() const {
+        Node* temp = head;
+        std::cout << "Show single list:" << std::endl;
+        while (temp != nullptr) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+    void pushFront(const T& value) {
+        Node* newNode = new Node(value);
+        newNode->next = head;
+        head = newNode;
+        ++size;
+    }
 
-	void DeleteFromEnd() {
-		if (head == nullptr) return;
-		if (head->GetNext() == nullptr)
-		{
-			delete head;
-			head = nullptr;
-			size--;
-			return;
-		}
-		Node* temp = head;
-		while (temp->GetNext()->GetNext() != nullptr)
-		{
-			temp = temp->GetNext();
-		}
-		delete temp->GetNext();
-		temp->SetNext(nullptr);
-		size--;
-	}
-
-	const MyInt& operator[](size_t index) {
-		if (index >= size) std::cout << "Index out of range" << std::endl;
-		Node* temp = head;
-		for (size_t i = 0; i < index; ++i)
-		{
-			temp = temp->GetNext();
-		}
-		return temp->GetMyInt();
-	}
-	Node * Begin() {
-		return head;
-	}
-	~LinkedList()
-	{
-		while (head != nullptr)
-		{
-			Node* temp = head;
-			head = head->GetNext();
-			delete temp;
-		}
-	}
+    void popFront() {
+        if (!head) {
+            std::cout << "List is empty" << std::endl;
+        }
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        --size;
+    }
+    bool isEmpty() const { return size == 0; }
+    size_t getSize() const { return size; }
 };
 
-int main()
-{
-	LinkedList* list = new LinkedList();
-	list->InsertAtBeginning(MyInt(5));
-	list->InsertAtEnd(7);
-	list->InsertAtEnd(4);
-	list->InsertAtEnd(3);
-	list->DeleteFromEnd();
-	list->DeleteFromBeginning();
-	std::cout << "Get element with index 1: " << list->operator[](1).GetData() << std::endl;
-	std::cout << "Loop through list: " << std::endl;
-	for (Node* it = list->Begin(); it != nullptr; it = it->GetNext()) {
-		if (it->GetNext() == nullptr) {
-			std::cout << it->GetMyInt().GetData();
-		}
-		else {
-			std::cout << it->GetMyInt().GetData() << " ";
-		}
-	}
+int main() {
+    Queue<int> q;
+    q.enqueue(1);
+    q.enqueue(2);
+    std::cout << "Queue front: " << q.peek() << std::endl;
+    std::cout << "Dequeue" << std::endl;
+    q.dequeue();
+    std::cout << "Now queue front: " << q.peek() << std::endl;
+    std::cout << "Create double list" << std::endl;
+    LinkedList<int> dll;
+    std::cout << "Fill double list" << std::endl;
+    dll.pushFront(10);
+    dll.pushBack(20);
+    dll.ShowList();
+    std::cout << "popFront method called" << std::endl;
+    dll.popFront();
+    dll.ShowList();
+    std::cout << "Create single list" << std::endl;
+    SingleLinkedList<int> sll;
+    sll.pushFront(5);
+    std::cout << "pushFront 5" << std::endl;
+    sll.ShowList();
+    std::cout << "popFront method called" << std::endl;
+    sll.popFront();
+    sll.ShowList();
+    return 0;
 }
