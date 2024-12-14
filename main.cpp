c2ed8fa1157d093cee4e0284d8fb8ebb50a4e99d
Artem Maltsev <artemmaltsevwork@gmail.com>
2024-12-14 22:52:20 +0200

    Homework#5

--- main.cpp
+++ main.cpp
@@ -1,228 +1,92 @@
 ﻿#include <iostream>
-#include <stdexcept>
+#include <string>
+using namespace std;
 
-// TEMPLATE QUEUE
-template <typename T>
-class Queue {
+// TASK1
+class Person {
 private:
-    class Node
-    {
-    private:
-        T data;
-        Node* next;
-        Node* prev;
-    public:
-        Node(T data) : data(data), next(nullptr), prev(nullptr) {}
-        T& GetData()
-        {
-            return data;
-        }
-        void SetData(T data)
-        {
-            this->data = data;
-        }
-        Node* GetNext() const
-        {
-            return next;
-        }
-        void SetNext(Node* next)
-        {
-            this->next = next;
-        }
-        Node* GetPrev() const
-        {
-            return prev;
-        }
-        void SetPrev(Node* prev)
-        {
-            this->prev = prev;
-        }
-    };
-    Node* front;
-    Node* rear;
-    size_t size;
-
+	string name;
+	int age;
 public:
-    Queue() : front(nullptr), rear(nullptr), size(0) {}
-    ~Queue() {
-        while (!isEmpty()) {
-            DequeueFront();
-        }
-    }
-    void EnqueueBack(const T& value) {
-        Node* newNode = new Node(value);
-        if (rear == nullptr) {
-            front = newNode;
-            rear = newNode;
-        }
-        else {
-            rear->SetNext(newNode);
-            newNode->SetPrev(rear);
-            rear = newNode;
-        }
-        ++size;
-    }
-
-    void EnqueueFront(const T& value) {
-        Node* newNode = new Node(value);
-        if (rear == nullptr) {
-            front = newNode;
-            rear = newNode;
-        }
-        else {
-            newNode->SetNext(front);
-            front->SetPrev(newNode);
-            front = newNode;
-        }
-        ++size;
-    }
-
-    T& Front() {
-        if (front == nullptr) {
-            throw std::runtime_error("Queue is empty!");
-        }
-        return front->GetData();
-    }
-
-    T Front() const {
-        return Front();
-    }
-
-    T& Back() {
-        if (rear == nullptr) {
-            throw std::runtime_error("Queue is empty!");
-        }
-        return rear->GetData();
-    }
-
-    T Back() const {
-        return Back();
-    }
-
-    void DequeueFront() {
-        if (front == nullptr) {
-            return;
-        }
-        Node* temp = front;
-        front = front->GetNext();
-        if (front == nullptr) {
-            rear = nullptr;
-        }
-        delete temp;
-        size--;
-    }
-
-    void DequeueBack() {
-        if (rear == nullptr) {
-            return;
-        }
-        Node* temp = rear;
-        rear = rear->GetPrev();
-        if (rear == nullptr) {
-            front = nullptr;
-        }
-        delete temp;
-        size--;
-    }
-
-    bool isEmpty() const { return size == 0; }
-    size_t Size() const { return size; }
+	Person(string name, int age) : name(name), age(age) {}
+	virtual ~Person() {}
+	virtual string getInfo() {
+		return "Name: " + name + ", Age: " + to_string(age);
+	}
 };
 
-
-template<typename T>
-class Stack {
+class Child : public Person {
 private:
-    class Node
-    {
-    private:
-        T data;
-        Node* next;
-    public:
-        Node(T data) : data(data), next(nullptr) {}
-        T& GetData()
-        {
-            return data;
-        }
-        void SetData(T data)
-        {
-            this->data = data;
-        }
-        Node* GetNext() const
-        {
-            return next;
-        }
-        void SetNext(Node* next)
-        {
-            this->next = next;
-        }
-    };
-    Node* top;
-    size_t size;
+	string parentName;
 public:
-    Stack() : top(nullptr), size(0) {}
-    size_t GetSize() const {
-        return size;
-    }
-    void Push(T data) {
-        Node* newNode = new Node(data);
-        newNode->SetNext(top);
-        top = newNode;
-        size++;
-    }
-
-    T& Peek() {
-        if (top == nullptr) {
-            throw std::runtime_error("Stack is empty!");
-        }
-        return top->GetData();
-    }
-    const T Peek() const {
-        return Peek();
-    }
-    void Pop() {
-        if (top == nullptr) {
-            throw std::runtime_error("Stack is empty");
-        }
-        Node* temp = top;
-        top = top->GetNext();
-        delete temp;
-        size--;
-    }
-    ~Stack() {
-        while (top != nullptr) {
-            Node* temp = top;
-            top = top->GetNext();
-            delete temp;
-        }
-    }
+	Child(string name, int age, string parentName) : Person(name, age), parentName(parentName) {}
+	string getInfo() {
+		return Person::getInfo() + ", Parent: " + parentName;
+	}
 };
 
+//TASK2
+class Vehicle {
+private:
+	string brand;
+	int year;
+public:
+	Vehicle(string brand, int year) : brand(brand), year(year) {};
+	virtual string getDescription() {
+		return "Brand: " + brand + ", Year" + to_string(year);
+	}
+	virtual ~Vehicle() {}
+};
+
+class Car : public Vehicle {
+private:
+	string model;
+	int mileage;
+public:
+	Car(string brand, int year, string model, int mileage) : Vehicle(brand, year), model(model), mileage(mileage) {}
+	~Car() {}
+	string getDescription() {
+		return Vehicle::getDescription() + ", Model: " + model + ", Mileage: " + to_string(mileage) + "km";
+	}
+};
+
+//TASK3
+class Employee {
+private:
+	string name;
+	string position;
+	double salary;
+public:
+	Employee(string name, string postion, double salary) : name(name), position(position), salary(salary) {}
+	virtual ~Employee() {}
+	virtual string getDetails() {
+		return "Name: " + name + ", Position: " + position + ", Salary: " + to_string(salary);
+	}
+};
+
+class Manager : public Employee {
+private:
+	string department;
+public:
+	Manager(string name, string position, double salary, string department) : Employee(name, position, salary), department(department) {};
+	~Manager() {}
+	string getDetails() {
+		return Employee::getDetails() + ", Department: " + department;
+	}
+};
 
 int main() {
-    Queue<int> queue;
-    queue.EnqueueBack(9);
-    queue.EnqueueBack(1);
-    queue.EnqueueBack(5);
-    queue.EnqueueBack(7);
-    queue.EnqueueBack(3);
-
-    for (size_t i = queue.Size(); i > 0; --i) {
-        std::cout << queue.Front() << std::endl;
-        queue.DequeueFront();
-    }
-    std::cout << std::endl;
-
-    Stack<int> stack;
-    stack.Push(1);
-    stack.Push(2);
-    stack.Push(6);
-    stack.Push(9);
-    stack.Push(123);
-    while (stack.GetSize() != 0) {
-        std::cout << stack.Peek() << std::endl;
-        stack.Pop();
-    }
-
-    return 0;
+	//TASK1
+	Person Test1Person("Testik", 32);
+	Child Test1Child("Test1", 12, "Tetsik");
+	cout << Test1Person.getInfo() << endl << Test1Child.getInfo() << endl;
+	//TASK2
+	Vehicle Test1Vehicle("BMW", 2022);
+	Car Test1Car("BMW", 2022, "IX8700", 400);
+	cout << Test1Vehicle.getDescription() << endl << Test1Car.getDescription() << endl;
+	//TASK3
+	Employee Test1Employee("Alice", "Developer", 50000.00);
+	Manager Test1Manager("Alice", "Developer", 50000.00, "IT");
+	cout << Test1Employee.getDetails() << endl << Test1Manager.getDetails() << endl;
+	return 0;
 }
