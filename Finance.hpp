b16e0b40b1c6391f1235355a0b841af5b6c1745f
Artem Maltsev <artemmaltsevwork@gmail.com>
2025-01-04 19:25:34 +0200

    Exam

---
+++ Finance.hpp
@@ -0,0 +1,139 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <fstream>
+#include <ctime>
+
+
+struct Transaction
+{
+	std::string type; // тип транзакции либо spend либо deposit
+	double value; // сумма сделки
+	std::string category; // категория в которую были потрачены деньги
+	time_t date; // сохранение времени, что бы можно было сделать отчет за определенное время 
+};
+
+
+class Card {
+private:
+	std::string name; // Название карты
+	std::string type; // Тип карты (дебитовая/кредитная)
+	double balance; // баланс на карте
+	std::vector<Transaction> transactions; // Транзакции, нужны что бы генерировать отчеты
+    // std::vector выбрал потому-что это очень удобный динамический массив 
+    // гораздо удобней использовать готовую структуру данных чем создавать свою динамическую коллекцию
+public:
+    Card() : name(""), type(""), balance(0.0) {} // конструктор по умолчанию
+    Card(const std::string& name, const std::string& type) : name(name), type(type), balance(0.0) {} // конструктор
+    void deposit(double value) {
+        balance += value; // пополняем баланс
+        transactions.push_back({ "deposit", value, "", std::time(nullptr) }); // вношу пополнение баланса в массив транзакций
+    }
+
+    void spend(double value, const std::string& category) {
+        if (value > balance) {
+            throw std::runtime_error("Insufficient balance"); // Выкидываю исключение, если средств на балансе недостаточно
+        }
+        balance -= value; // Отнимаю баланс
+        transactions.push_back({ "spend", value, category, std::time(nullptr) }); // вношу перевод средств в массив транзацкий, для формирования отчетов в дальнейшем
+    }
+
+    const std::vector<Transaction>& getTransactions() const {
+        return transactions; // getter метод для получения вектора транзакций для генерации отчетов
+    }
+};
+
+
+class Customer {
+private:
+    std::map<std::string, Card> cards; // словарь карт (ключ: значение), карты клиента
+    time_t calculateDate(const std::string& period) {
+        time_t now = std::time(nullptr); // Создаю объект time_t с значением акутального времени
+        if (period == "day") {
+            return now - 24 * 60 * 60;
+        }
+        else if (period == "week") {
+            return now - 7 * 24 * 60 * 60;
+        }
+        else if (period == "month") {
+            return now - 30 * 24 * 60 * 60;
+        }
+        else {
+            throw std::invalid_argument("Invalid period"); // Кидаю исключения, о нправильно переданном аргументе
+        }
+    }
+
+public:
+    void addCard(const std::string& name, const std::string& cardType) {
+        if (cards.find(name) != cards.end()) { // Проверка, если карта с таким именем есть, то выкидываем исключение
+            throw std::runtime_error("Card already exists"); // Кидаю исключение, что карта с таким именем уже есть
+        }
+        cards[name] = Card(name, cardType); // Добавляю в словарь карт, карту с именем name
+    }
+
+    void depositToCard(const std::string& name, double value) {
+        if (cards.find(name) == cards.end()) { // Проверка, если карты с таким именем нету выкидываю исключение
+            throw std::runtime_error("Card does not exist"); // Кидаю исключение, карты с таким именем нету
+        }
+        cards[name].deposit(value); // Вношу депозит на карту
+    }
+
+    void spendFromCard(const std::string& name, double value, const std::string& category) {
+        if (cards.find(name) == cards.end()) { // Проверка, если карты с таким именем нету выкидываю исключение
+            throw std::runtime_error("Card does not exist"); // Кидаю исключение, карты с таким именем нету
+        }
+        cards[name].spend(value, category); // Перевожу с карты
+    }
+
+    std::map<std::string, double> generateReport(const std::string& period) {
+        std::time_t startDate = calculateDate(period); // Беру дату за которую нужно сгенерировать отчет
+        std::time_t now = std::time(nullptr); // Беру актуальное время
+        std::map<std::string, double> report; // Создаю словарь отчета
+        for (const auto& card : cards) { // Прохожусь по всем картам клиента
+            const auto& transactions = card.second.getTransactions(); // Получаю список транзакций конркретной карты
+            for (const auto& transaction : transactions) { // Прохожусь по транзакциям конркретной карты клиента
+                if (transaction.type == "spend" && transaction.date >= startDate && transaction.date <= now) { // Проверяю транзакцию
+                    report[transaction.category] += transaction.value; // Добавляю сколько денег было потраченно на категорию
+                }
+            }
+        }
+        return report; // Возврщаю отчет
+    }
+
+    std::vector<Transaction> generateTop(const std::string& period) {
+        std::time_t startDate = calculateDate(period); // Беру дату за которую нужно сгенерировать отчет
+        std::time_t now = std::time(nullptr); // Беру актуальное время
+        std::vector<Transaction> top; // Создаю словарь топа
+        for (const auto& card : cards) { // Прохожусь по всем картам клиента
+            const auto& transactions = card.second.getTransactions(); // Получаю список транзакций конркретной карты
+            for (const auto& transaction : transactions) { // Прохожусь по транзакциям конркретной карты клиента
+                if (transaction.type == "spend" && transaction.date >= startDate && transaction.date <= now) { // Проверяю транзакцию
+                    top.push_back(transaction); // Добавляю транзакцию в топ
+                }
+            }
+        }
+        std::sort(top.begin(), top.end(), [](const Transaction& a, const Transaction& b) { // сортирую вектор top по лямбда-функции
+            // Использую лямбда функцию, для сортировки, сравниваю значения сум транзакций
+            return a.value > b.value;
+            }
+        );
+
+        if (top.size() > 3) { // Проверяю больше ли записей в топе чем 3 штуки
+            top.resize(3); // Если да, то изменяю размер вектора, что бы было только 3
+        }
+        return top; // Возвращаю вектор топа
+    }
+
+    void saveReportToFile(const std::map<std::string, double>& report, const std::string& filename) {
+        std::ofstream file(filename); // Создаю объект записи а файл
+        if (!file.is_open()) { // Проверяю открыт ли файл
+            throw std::runtime_error("Failed to open file"); // Кидаю исключение, если файл не открыт
+        }
+        for (const auto& pair : report) { // Прохожусь по отчету
+            file << pair.first << ": " << pair.second << "\n"; // Добавляю записи в файл
+        }
+        file.close(); // Закрываю файл
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,231 +1,102 @@
-﻿#include <iostream>
-#include <string>
-#include <stdexcept>
+﻿#include "Finance.hpp"
 
-using namespace std;
-
-class Engine {
-private:
-    double volume;
-    string fuelType;
-    int horsepower;
-public:
-    Engine(double volume, string fuelType, int horsepower) : volume(volume), fuelType(fuelType), horsepower(horsepower) {}
-    double getVolume() const { return volume; }
-    string getFuelType() const { return fuelType; }
-    int getPower() const { return horsepower; }
-    void displayDetails() const {
-        cout << "Engine:\nVolume = " << volume << "L\nFuel Type = " << fuelType << "\nPower = " << horsepower << "HP\n";
-    }
-    bool operator==(const Engine& other) const {
-        return volume == other.volume && fuelType == other.fuelType && horsepower == other.horsepower;
-    }
-};
-
-
-class Transmission {
-private:
-    string type;
-public:
-    Transmission(string type) : type(type) {}
-    string getType() const { return type; }
-    void displayDetails() const {
-        cout << "Transmission Type = " << type << "\n";
-    }
-    bool operator==(const Transmission& other) const {
-        return type == other.type;
-    }
-};
-
-
-class Vehicle {
-protected:
-    string type;
-    string brand;
-    string model;
-    int year;
-    double price;
-    Engine engine;
-    Transmission transmission;
-
-public:
-    Vehicle(string type, string brand, string model, int year, double price, Engine engine, Transmission transmission) : type(type), brand(brand), model(model), year(year), price(price), engine(engine), transmission(transmission) {}
-    virtual ~Vehicle() {}
-    string getTypeVehicle() const { return type; }
-    string getBrand() const { return brand; }
-    string getModel() const { return model; }
-    int getYear() const { return year; }
-    double getPrice() const { return price; }
-    Engine getEngine() const { return engine; }
-    Transmission getTransmission() const { return transmission; }
-    virtual void displayDetails() const {
-        cout << "Vehicle Type = " << type << ", Brand = " << brand << ", Model = " << model << ", Year = " << year << ", Price = " << price << "$\n";
-        engine.displayDetails();
-        transmission.displayDetails();
-    }
-};
-
-
-class Sedan : public Vehicle {
-private:
-    bool hatch;
-public:
-    Sedan(string brand, string model, int year, double price, Engine engine, Transmission transmission, bool hatch) : Vehicle("Sedan", brand, model, year, price, engine, transmission), hatch(hatch) {}
-    void displayDetails() const override {
-        Vehicle::displayDetails();
-        cout << "Hatch: " << (hatch ? "Yes" : "No") << "\n";
-    }
-};
-
-
-class Truck : public Vehicle {
-private:
-    float weight_power;
-public:
-    Truck(string brand, string model, int year, double price, Engine engine, Transmission transmission, float weight_power) : Vehicle("Truck", brand, model, year, price, engine, transmission), weight_power(weight_power) {}
-    void displayDetails() const override {
-        Vehicle::displayDetails();
-        cout << "Weight power: " << weight_power << " tons\n";
-    }
-};
-
-
-class Motorcycle : public Vehicle {
-private:
-    double fuel_waste;
-
-public:
-    Motorcycle(string brand, string model, int year, double price, Engine engine, Transmission transmission, double fuel_waste) : Vehicle("Motorcycle", brand, model, year, price, engine, transmission), fuel_waste(fuel_waste) {}
-
-    void displayDetails() const override {
-        Vehicle::displayDetails();
-        cout << "Fuel Consumption: " << fuel_waste << " L/100km\n";
-    }
-};
-
-
-class Moped : public Vehicle {
-private:
-    double max_speed;
-
-public:
-    Moped(string brand, string model, int year, double price, Engine engine, Transmission transmission, double max_speed)
-        : Vehicle("Moped", brand, model, year, price, engine, transmission), max_speed(max_speed) {}
-
-    void displayDetails() const override {
-        Vehicle::displayDetails();
-        cout << "Max Speed: " << max_speed << " km/h\n";
-    }
-};
-
-
-Vehicle** SearchByType(Vehicle** vehicles, int size, const string& type, int& resultSize) {
-    Vehicle** results = new Vehicle * [size];
-    resultSize = 0;
-    for (int i = 0; i < size; ++i) {
-        if (vehicles[i]->getTypeVehicle() == type) {
-            results[resultSize++] = vehicles[i];
-        }
-    }
-    Vehicle** resizedResults = new Vehicle * [resultSize];
-    for (int i = 0; i < resultSize; ++i) {
-        resizedResults[i] = results[i];
-    }
-    delete[] results;
-    return resizedResults;
+void printMenu() {
+    std::cout << "\nMenu" << std::endl;
+    std::cout << "1. Add Card" << std::endl;
+    std::cout << "2. Deposit to Card" << std::endl;
+    std::cout << "3. Spend from Card" << std::endl;
+    std::cout << "4. Generate Report" << std::endl;
+    std::cout << "5. Generate Top" << std::endl;
+    std::cout << "6. Save Report to File" << std::endl;
+    std::cout << "7. Exit" << std::endl;
+    std::cout << "Select an option: ";
 }
-
-
-Vehicle** SearchByYearProd(Vehicle** vehicles, int size, int year, int& resultSize) {
-    Vehicle** results = new Vehicle* [size];
-    resultSize  = 0;
-    for (int i = 0; i < size; ++i) {
-        if (vehicles[i]->getYear() == year) {
-            results[resultSize++] = vehicles[i];
-        }
-    }
-    Vehicle** resizedResults = new Vehicle* [resultSize];
-    for (int i = 0; i < resultSize; ++i) {
-        resizedResults[i] = results[i];
-    }
-    delete[] results;
-    return resizedResults;
-}
-
-
-Vehicle** SearchByPrice(Vehicle** vehicles, int size, double minPrice, double maxPrice, int& resultSize) {
-    Vehicle** results = new Vehicle * [size];
-    resultSize = 0;
-    for (int i = 0; i < size; ++i) {
-        double vehicle_price = vehicles[i]->getPrice();
-        if (vehicle_price >= minPrice && vehicle_price <= maxPrice) {
-            results[resultSize++] = vehicles[i];
-        }
-    }
-    Vehicle** resizedResults = new Vehicle * [resultSize];
-    for (int i = 0; i < resultSize; ++i) {
-        resizedResults[i] = results[i];
-    }
-    delete[] results;
-    return resizedResults;
-}
-
-
-Vehicle** SearchBySeveralParameters(Vehicle** vehicles, int size, const string& brand, const string& model, int year, int& resultSize) {
-    Vehicle** results = new Vehicle * [size];
-    resultSize = 0;
-    for (int i = 0; i < size; ++i) {
-        if (vehicles[i]->getBrand() == brand && vehicles[i]->getModel() == model && vehicles[i]->getYear() == year) {
-            results[resultSize++] = vehicles[i];
-        }
-    }
-    Vehicle** resizedResults = new Vehicle * [resultSize];
-    for (int i = 0; i < resultSize; ++i) {
-        resizedResults[i] = results[i];
-    }
-    delete[] results;
-    return resizedResults;
-}
-
 
 int main() {
-    int size = 4;
-    Vehicle** vehicles = new Vehicle * [size];
-    vehicles[0] = new Sedan("Toyota", "Camry", 2020, 30000, Engine(2.5, "Petrol", 203), Transmission("Automatic"), true);
-    vehicles[1] = new Truck("Ford", "F-150", 2018, 40000, Engine(3.5, "Diesel", 375), Transmission("Manual"), 5.0);
-    vehicles[2] = new Motorcycle("Yamaha", "MT-07", 2021, 7500, Engine(0.7, "Petrol", 74), Transmission("Manual"), 4.5);
-    vehicles[3] = new Moped("Honda", "Dio", 2019, 1500, Engine(0.1, "Petrol", 8), Transmission("Automatic"), 90);
-    for (int i = 0; i < size; ++i) {
-        vehicles[i]->displayDetails();
-    }
-    int sedansResultSize = 0;
-    Vehicle** sedans = SearchByType(vehicles, size, "Sedan", sedansResultSize);
-    cout << "\nSedans:\n";
-    for (int i = 0; i < sedansResultSize; ++i) {
-        sedans[i]->displayDetails();
-    }
-    int yearResultSize = 0;
-    Vehicle** year2021 = SearchByYearProd(vehicles, size, 2021, yearResultSize);
-    cout << "\nVehicles from 2021:\n";
-    for (int i = 0; i < yearResultSize; ++i) {
-        year2021[i]->displayDetails();
-    }
-    int priceResultSize = 0;
-    Vehicle** priceRange = SearchByPrice(vehicles, size, 2000, 35000, priceResultSize);
-    cout << "\nVehicles priced between $2000 and $35000:\n";
-    for (int i = 0; i < priceResultSize; ++i) {
-        priceRange[i]->displayDetails();
-    }
-    int advanceSearchingResultSize = 0;
-    Vehicle** advanceSearching = SearchBySeveralParameters(vehicles, size, "Ford", "F-150", 2018, advanceSearchingResultSize);
-    cout << "\nVehicles  advance searching:\n";
-    for (int i = 0; i < advanceSearchingResultSize; ++i) {
-        advanceSearching[i]->displayDetails();
-    }
-    for (int i = 0; i < size; ++i) {
-        delete vehicles[i];
-    }
-    delete[] vehicles;
+    Customer user;
+    int choice;
+
+    while (true) {
+        printMenu();
+        std::cin >> choice;
+        switch (choice) {
+        case 1: {
+            std::string name, type;
+            std::cout << "Enter card name: ";
+            std::cin >> name;
+            std::cout << "Enter card type (debit/credit): ";
+            std::cin >> type;
+            user.addCard(name, type);
+            std::cout << "Card added successfully" << std::endl;
+            break;
+        }
+        case 2: {
+            std::string name;
+            double value;
+            std::cout << "Enter card name: ";
+            std::cin >> name;
+            std::cout << "Enter amount to deposit: ";
+            std::cin >> value;
+            user.depositToCard(name, value);
+            std::cout << "Deposit successful" << std::endl;
+            break;
+        }
+        case 3: {
+            std::string name, category;
+            double value;
+            std::cout << "Enter card name: ";
+            std::cin >> name;
+            std::cout << "Enter amount to spend: ";
+            std::cin >> value;
+            std::cout << "Enter category: ";
+            std::cin >> category;
+            user.spendFromCard(name, value, category);
+            std::cout << "Transaction successful" << std::endl;
+            break;
+        }
+        case 4: {
+            std::string period;
+            std::cout << "Enter report period (day/week/month): ";
+            std::cin >> period;
+            auto report = user.generateReport(period);
+            std::cout << "\nReport:\n";
+            for (const auto& pair : report) {
+                std::cout << pair.first << ": " << pair.second << std::endl;
+            }
+            break;
+        }
+        case 5: {
+            std::string period;
+            std::cout << "Enter period for top (day/week/month): ";
+            std::cin >> period;
+            auto top = user.generateTop(period);
+            std::cout << "\nTop Expenses:\n";
+            for (const auto& transaction : top) {
+                std::cout << "Category: " << transaction.category << ", Value: " << transaction.value << std::endl;
+            }
+            break;
+        }
+        case 6: {
+            std::string period, filename;
+            std::cout << "Enter report period (day/week/month): ";
+            std::cin >> period;
+            std::cout << "Enter filename: ";
+            std::cin >> filename;
+            auto report = user.generateReport(period);
+            user.saveReportToFile(report, filename);
+            std::cout << "Report saved to file successfully" << std::endl;
+            break;
+        }
+        case 7: {
+            std::cout << "Exiting program" << std::endl;
+            return 0;
+        }
+        default: {
+            std::cout << "Invalid option. Please try again" << std::endl;
+            break;
+        }
+     }
+}
 
     return 0;
 }
